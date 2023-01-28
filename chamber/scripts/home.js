22b00ef4244de007e0db4a5242c42a48332522dd
laurelcerrato <laurelcerrato@gmail.com>
2023-01-27 18:18:23 -0600

    finished small view css

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -1,3 +1,17 @@
 const year = document.querySelector(".year");
 year.textContent = new Date().getFullYear();
 document.getElementById("date").textContent = document.lastModified
+// nav js
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x =document.getElementById("hamburgerBtn");
+x.onclick= toggleMenu;
+
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+datefield.innerHTML = `<em>${fulldateUK}</em>`;
