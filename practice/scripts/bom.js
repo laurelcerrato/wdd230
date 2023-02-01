57cbed3c74490e1dbdcaa7636194a7f93cb48558
laurelcerrato <laurelcerrato@gmail.com>
2023-02-01 02:11:34 -0600

    bom

---
+++ practice/scripts/bom.js
@@ -0,0 +1,19 @@
+const ul = document.querySelector("ul");
+const input =document.querySelector("input");
+const button = document.querySelector("button");
+button.addEventListener('click', () =>{
+    let inputV = input.value;
+    input.value= " ";
+
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    li.textContent= inputV;
+    button.textContent= "❌";
+    li.appendChild(button);
+    ul.appendChild(li);
+    button.addEventListener('click', () => {
+        ul.removeChild(li);
+        
+    });
+    input.focus();
+});
