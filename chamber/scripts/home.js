172e62c0414d49d908a7fa8eeab1167f7c06f5ba
laurelcerrato <laurelcerrato@gmail.com>
2023-02-03 20:50:52 -0600

    created banner and the js

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -1,20 +1,16 @@
+//last modified js
 const year = document.querySelector(".year");
 year.textContent = new Date().getFullYear();
 document.getElementById("date").textContent = document.lastModified
-// // nav js
-// function toggleMenu() {
-//     document.getElementById("primaryNav").classList.toggle("open");
-//     document.getElementById("hamburgerBtn").classList.toggle("open");
-// }
-// const x =document.getElementById("hamburgerBtn");
-// x.onclick= toggleMenu;
 
+//date js
 const datefield = document.querySelector(".date");
 const now = new Date();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
 datefield.innerHTML = `<em>${fulldateUK}</em>`;
+//nav js
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("#primaryNav");
 hamburger.addEventListener("click", () => {
@@ -22,3 +18,15 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 
 })
+//Banner js
+const banner = document.querySelector(".banner");
+//using the now variable that holds the current date
+if (now == 1 || now == 2){
+    banner.style.display = "block";
+}else{
+    banner.style.display= "none";
+}
+document.querySelector(".banner__close").addEventListener("click", function () {
+    this.closest(".banner").style.display = "none";
+  });
+  
