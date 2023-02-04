68e89b5c6cd13d2684d40f8aad7ef6114b1eb304
laurelcerrato <laurelcerrato@gmail.com>
2023-02-03 21:40:31 -0600

    last changes

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -29,5 +29,4 @@ if (dayOfW == 1 || dayOfW == 2){
 }
 document.querySelector(".banner__close").addEventListener("click", function () {
     this.closest(".banner").style.display = "none";
-  });
-  
+});
