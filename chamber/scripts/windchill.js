956b2753af8bd589cb8bd7288c70127b13f1639e
laurelcerrato <laurelcerrato@gmail.com>
2023-02-16 19:53:50 -0600

    fixed windchill error

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,7 +4,7 @@ let windchill = document.querySelector(".wind-chill");
 let celciustofarenh = (temp.innerHTML * 9/5)+ 32;
 let mph = speed.innerHTML / 1.609344;
 let finalwindchill;
-if (celciustofarenh <= 50 && mph > 30){
+if (celciustofarenh <= 50 && mph > 3.0){
     finalwindchill= 35.75 + (0.6215 * temp.innerHTML) - (35.75*(Math.pow(speed.innerHTML,0.16))) + (0.4275 * temp.innerHTML * (Math.pow(speed.innerHTML, 0.16)));
 }else{
     finalwindchill= "N/A";
