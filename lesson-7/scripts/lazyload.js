13a3c50b006a69f0a94ff9fa9ce948d27f84cfd9
laurelcerrato <laurelcerrato@gmail.com>
2023-02-19 18:43:45 -0600

    done

--- lesson-7/scripts/lazyload.js
+++ lesson-7/scripts/lazyload.js
@@ -1,34 +1,26 @@
-
-const images = document.querySelectorAll("img[data-src]")
+let imagesToLoad = document.querySelectorAll("[data-src]");
+const loadImage = (img) => {
+    const src = img.getAttribute("data-src");
+    img.src = src;
+    img.onload = () => {
+    img.removeAttribute("data-src");
+    img.parentElement.classList.remove("scale");
+    };
+}
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+    rootMargin: "0px 0px -200px 0px"
 };
-const load = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src")};
-        image.className = "none";
-    }
-if ('IntersectionObserver' in window) {
-    const imageOb= new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                load(item.target)
-                observer.unobserve(item.target)
-            }
-        })
-        }, imgOptions)
-    images.forEach((img) => {
-        imageOb.observe(img)
+const imgObserver = new IntersectionObserver((entries,imgObserver) => {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting){
+            return;
+        }else{
+            loadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
     })
-}
-else {
-    images.forEach((img) => {
-        load(img)
-    })
-}
-window.onload = function() { var placeholder = document.querySelector('data-src'), small = placeholder.querySelector('.img-small') // 1: load small image and show it
-var img = new Image(); img.src = small.src; img.onload = function () { small.classList.add('loaded'); };
-// 2: load large image
-var imgLarge = new Image(); imgLarge.src = placeholder.dataset.large; imgLarge.onload = function () { imgLarge.classList.add('loaded'); }; placeholder.appendChild(imgLarge);}
+}, imgOptions);
+imagesToLoad.forEach(image => {
+    imgObserver.observe(image);
+});
