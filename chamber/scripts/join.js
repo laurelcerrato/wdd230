029dd12394d9fe0b7e7a40144554322c32c7112a
laurelcerrato <laurelcerrato@gmail.com>
2023-02-25 23:48:21 -0600

    finish large view

--- chamber/scripts/join.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-        var inputValue = $(this).attr("value");
-        var targetBox = $("." + inputValue);
-        $(".box").not(targetBox).hide();
-        $(targetBox).show();
-    });
-});
