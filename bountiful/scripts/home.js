ff7420364c7b159b84d213940efd262504981699
laurelcerrato <laurelcerrato@gmail.com>
2023-04-04 00:40:54 -0600

    added nav bar

---
+++ bountiful/scripts/home.js
@@ -0,0 +1,8 @@
+//nav js
+const hamburger = document.getElementById("toggle-button");
+const navMenu = document.getElementById("navi-list");
+hamburger.addEventListener("click", () => {
+    // hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+})
