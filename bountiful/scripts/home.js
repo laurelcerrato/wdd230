c443a422e4adbca8b17749d7c30adbb276f7cca5
laurelcerrato <laurelcerrato@gmail.com>
2023-04-04 18:42:10 -0600

    finished nav header and footer

--- bountiful/scripts/home.js
+++ bountiful/scripts/home.js
@@ -6,3 +6,15 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 
 })
+//date js
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+datefield.innerHTML = `<em>${fulldateUK}</em>`;
+
+//last modified js
+const year = document.querySelector(".year");
+year.textContent = new Date().getFullYear();
+document.getElementById("date").textContent = document.lastModified
