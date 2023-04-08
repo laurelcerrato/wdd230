af052f4d6f664cd77fb8879f0b61b160cf429171
laurelcerrato <laurelcerrato@gmail.com>
2023-04-08 17:33:53 -0600

    finish

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -5,47 +5,51 @@ async function getfruitsData() {
     displayOptions(data);
     //getNutrition(data);
 }
+getfruitsData();
+
+
+const formElement = document.forms[0];
+
 const fruit1 = document.querySelector(".fruits1");
 const fruit2 = document.querySelector(".fruits2");
 const fruit3 = document.querySelector(".fruits3");
 //<option value="english">English</option>
-getfruitsData()
 const displayOptions = (fruits) =>{
-    fruits.forEach(fruit => {
+    console.log(fruits[0].nutritions);
+
+    for(let i = 0; i < fruits.length; i++){ 
+        console.log(fruits[i]);
         let option = document.createElement('option');
-        option.textContent = fruit.name;
-        option.setAttribute("value",fruit.name);
+        option.textContent = fruits[i].name;
+        option.setAttribute("value",fruits[i].name);
+
+        let option1car= option.setAttribute("carbohydrates",fruits[i].nutritions.carbohydrates);
+        let option1cal = option.setAttribute("calories",fruits[i].nutritions.calories);
+        let option1fat = option.setAttribute("fat",fruits[i].nutritions.fat);
+        let option1pro = option.setAttribute("pro",fruits[i].nutritions.protein);
+
         let option2 = document.createElement('option');
-        option2.textContent = fruit.name;
-        option2.setAttribute("value",fruit.name);
+        option2.textContent = fruits[i].name;
+        option2.setAttribute("value",fruits[i].name);
+
+        let option2car= option2.setAttribute("carbohydrates",fruits[i].nutritions.carbohydrates);
+        let option2cal = option2.setAttribute("calories",fruits[i].nutritions.calories);
+        let option2fat = option2.setAttribute("fat",fruits[i].nutritions.fat);
+        let option2pro = option2.setAttribute("pro",fruits[i].nutritions.protein);
+
         let option3 = document.createElement('option');
-        option3.textContent = fruit.name;
-        option3.setAttribute("value",fruit.name);
+        option3.textContent = fruits[i].name;
+        option3.setAttribute("value",fruits[i].name);
+        let option3car= option3.setAttribute("carbohydrates",fruits[i].nutritions.carbohydrates);
+        let option3cal = option3.setAttribute("calories",fruits[i].nutritions.calories);
+        let option3fat = option3.setAttribute("fat",fruits[i].nutritions.fat);
+        let option3pro = option3.setAttribute("pro",fruits[i].nutritions.protein);
         fruit1.appendChild(option);
         fruit2.appendChild(option2);
         fruit3.appendChild(option3);
-        
-    });
-}
-//nutrition info
-const calories = 0;
-const fat= 0;
-const protein = 0;
-const carbohydrates= 0;
-const formElement = document.forms[0];
-const formfruit1 = formElement.fruit1.value;
-const formfruit2 = formElement.fruit2.value;
-const formfruit3 = formElement.fruit3.value;
-
-const getNutrition = (data) => {
-    data.forEach(fruit =>{
-        const fruitname = fruit.name;
-        if(fruitname == formfruit1){
-            carbohydrates += fruit.nutritions.carbohydrates;
-            console.log(carbohydrates);
         }
-    });
-}
+
+//localstorage
 const drinks = document.querySelector(".drinks");
 let amountofdrinks = Number(window.localStorage.getItem("amount"));
 formElement.addEventListener('submit', function(e) {
@@ -57,8 +61,14 @@ formElement.addEventListener('submit', function(e) {
 const feedbackElement = document.getElementById('feedback');
     formElement.addEventListener('submit', function(e) {
     e.preventDefault();
+
+    const carbohydrates = Math.ceil(Number(fruit1.options[fruit1.selectedIndex].getAttribute("carbohydrates")) + Number(fruit3.options[fruit3.selectedIndex].getAttribute("carbohydrates")) + Number(fruit2.options[fruit2.selectedIndex].getAttribute("carbohydrates")));
+    const calories = Math.ceil(Number(fruit1.options[fruit1.selectedIndex].getAttribute("calories")) + Number(fruit3.options[fruit3.selectedIndex].getAttribute("calories")) + Number(fruit2.options[fruit2.selectedIndex].getAttribute("calories")));
+    const protein = Math.ceil(Number(fruit1.options[fruit1.selectedIndex].getAttribute("pro")) + Number(fruit3.options[fruit3.selectedIndex].getAttribute("pro")) + Number(fruit2.options[fruit2.selectedIndex].getAttribute("pro")));
+    const fat = Math.ceil(Number(fruit1.options[fruit1.selectedIndex].getAttribute("fat")) + Number(fruit3.options[fruit3.selectedIndex].getAttribute("fat")) + Number(fruit2.options[fruit2.selectedIndex].getAttribute("fat"))); 
+
     feedbackElement.innerHTML = `Name: ${formElement.fname.value}<br>Email: ${formElement.email.value}<br>Phone: ${formElement.phone.value}<br> Selected fruits: ${formElement.fruit2.value}, ${formElement.fruit1.value}, ${formElement.fruit3.value} <br>Special Instructions: ${formElement.instructions.value} <br> Date: ${new Date().toDateString()} <br> Total Amount of Carbohydrates: ${carbohydrates} <br> Total Amount of protein: ${protein} <br> Total Amount of fat: ${fat} <br> Total Amount of calories: ${calories} `;
     feedbackElement.style.display = "block";
     feedbackElement.style.border= "1px solid #333";
     feedbackElement.style.borderRadius = "20px";
-        })
+        })}
--- bountiful/scripts/home.js
+++ bountiful/scripts/home.js
@@ -84,3 +84,4 @@ function displayresults(weatherData) {
     }
 
 
+
