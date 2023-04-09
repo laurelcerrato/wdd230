8e61a80bce2565d69734cba2e9e15702ee43d34f
laurelcerrato <laurelcerrato@gmail.com>
2023-04-08 19:07:02 -0600

    lazy

--- bountiful/scripts/home.js
+++ bountiful/scripts/home.js
@@ -44,6 +44,31 @@ function displayresults(weatherData) {
                             <p>${new Date((weatherData.list[17].dt)*1000).toDateString()} - Temperature: ${weatherData.list[17].main.temp} °F</p>
                             `;
     }
-
+//lazy
+let imagesToLoad = document.querySelectorAll("[data-src]");
+const loadImage = (img) => {
+    const src = img.getAttribute("data-src");
+    img.src = src;
+    img.onload = () => {
+    img.removeAttribute("data-src");
+    };
+}
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -200px 0px"
+};
+const imgObserver = new IntersectionObserver((entries,imgObserver) => {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting){
+            return;
+        }else{
+            loadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+imagesToLoad.forEach(image => {
+    imgObserver.observe(image);
+});
 
 
